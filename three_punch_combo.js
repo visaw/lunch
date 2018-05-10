09a0419f0db5c8f1f78833333e26acee7fad1534
visaw <704830827@qq.com>
2018-05-10 20:28:41 +0800

    first01

---
+++ three_punch_combo.js
@@ -0,0 +1,12 @@
+        $i = 0;
+        $('#start').click(function(){
+            $i++;
+            if($i >=6 ){
+                $('#start').hide();
+                $('#stop').show();
+            }
+        })
+        $('#stop').click(function(){
+            alert('这么作？要不吃千千吧！');
+            $(this).hide();
+        })
